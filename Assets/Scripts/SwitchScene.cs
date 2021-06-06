58f71560acf3b4d941f6697a08569853ebf96e6c
linkouille <linkouillou@gmail.com>
2021-06-06 19:13:02 +0200

    Some Fix and end tp

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}

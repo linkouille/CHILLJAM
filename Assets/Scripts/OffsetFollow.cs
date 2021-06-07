7cc454ba3f35e1d84e52072d61516a1ee182977f
Horace <horace@thepixelhunt.com>
2021-06-07 08:37:25 +0200

    Better bg

--- Assets/Scripts/OffsetFollow.cs
+++ Assets/Scripts/OffsetFollow.cs
@@ -7,9 +7,11 @@ public class OffsetFollow : MonoBehaviour
     [Range(0, 2)]
     public float rangeOffset = 0.8f;
 
+    public Vector3 startPos = new Vector3(200, 200, 0);
+
     // Update is called once per frame
     void LateUpdate()
     {
-        this.transform.position = this.transform.parent.position * rangeOffset;
+        this.transform.position = startPos + (this.transform.parent.position - startPos) * rangeOffset;
     }
 }

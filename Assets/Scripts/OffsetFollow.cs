15534c2f8a874651087f0a371ce0de4d93c65447
Horace <horace@thepixelhunt.com>
2021-06-07 08:58:06 +0200

    BG right direction

--- Assets/Scripts/OffsetFollow.cs
+++ Assets/Scripts/OffsetFollow.cs
@@ -6,12 +6,15 @@ public class OffsetFollow : MonoBehaviour
 {
     [Range(0, 2)]
     public float rangeOffset = 0.8f;
+    public float zPos = 0;
 
     public Vector3 startPos = new Vector3(200, 200, 0);
 
     // Update is called once per frame
     void LateUpdate()
     {
-        this.transform.position = startPos + (this.transform.parent.position - startPos) * rangeOffset;
+        Vector3 pos = startPos + (this.transform.parent.position - startPos) * rangeOffset;
+        pos.z = zPos;
+        this.transform.position = pos; 
     }
 }
